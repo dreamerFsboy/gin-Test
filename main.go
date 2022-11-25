471c46bbc3c968610a28946b26183ace0d0394ca
xvejy <920868587@qq.com>
2022-11-25 20:34:15 +0800

    添加路由

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"gin-Test/routers"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 
 	"github.com/dreamerFsboy/gin-Test/pkg/setting"
+	"github.com/dreamerFsboy/gin-Test/routers"
 )
 
 func main() {
-	router := gin.Default()
+	router := routers.InitRouter()
 	router.GET("/", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "test",
