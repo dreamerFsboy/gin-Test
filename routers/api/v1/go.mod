94a3930ab4bbc585417510eb279a3dea04f3871c
xvejy <920868587@qq.com>
2022-11-25 20:55:27 +0800

    api编写

---
+++ routers/api/v1/go.mod
@@ -0,0 +1,3 @@
+module gin-Test/routers/api/v1
+
+go 1.18
--- routers/router.go
+++ routers/router.go
@@ -1,19 +1,29 @@
 package routers
 
 import (
+	v1 "gin-Test/routers/api/v1"
+
 	"github.com/dreamerFsboy/gin-Test/pkg/setting"
+
 	"github.com/gin-gonic/gin"
 )
 
 func InitRouter() *gin.Engine {
 	r := gin.New()
+
 	r.Use(gin.Logger())
+
 	r.Use(gin.Recovery())
+
 	gin.SetMode(setting.RunMode)
-	r.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+
+	apiv1 := r.Group("/api/v1")
+	{
+		apiv1.GET("/tags", v1.GetTags)
+		apiv1.GET("/tags", v1.AddTag)
+		apiv1.GET("/tags", v1.DeleteTags)
+		apiv1.GET("/tags", v1.EditTags)
+	}
+
 	return r
 }
